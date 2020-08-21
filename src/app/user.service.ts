ba9126a20e54efe0823e813767b35a354fab5c09
= <=varuninorbit@yahoo.co.in>
2020-08-21 07:59:31 +0530

    User changed to IUser

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,35 +1,36 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { Observable} from "rxjs";
+import { Observable } from "rxjs";
 import { Urls } from '../environments/environment';
-import { User } from "./_models";
+import { IUser } from './_models';
+
 
 @Injectable()
 export class UserService {
-    Ouser: Observable<any>;
-    user: any; //TODO change type
-    constructor(private http: HttpClient){
+    Ouser: Observable<IUser>;
+    user: IUser; //TODO change type
+    constructor(private http: HttpClient) {
         this.Ouser = this.http
-        .get<Observable<any>>(Urls.host+'user/this') //TODO change <any> to user
-      }
+            .get<IUser>(Urls.host + 'user/this') //TODO change <any> to user
+    }
 
-  getAll() {
-      return this.http.get<User[]>(`${Urls.host}/users`);
-  }
+    getAll() {
+        return this.http.get<IUser[]>(`${Urls.host}/users`);
+    }
 
-  getById(id: number) {
-      return this.http.get(`${Urls.host}users/` + id);
-  }
+    getById(id: number) {
+        return this.http.get(`${Urls.host}users/` + id);
+    }
 
-  register(user: User) {
-      return this.http.post(`${Urls.host}users/register`, user);
-  }
+    register(user: IUser) {
+        return this.http.post(`${Urls.host}users/register`, user);
+    }
 
-  update(user: User) {
-      return this.http.put(`${Urls.host}users/` + user.id, user);
-  }
+    update(user: IUser) {
+        return this.http.put(`${Urls.host}users/` + user.id, user);
+    }
 
-  delete(id: number) {
-      return this.http.delete(`${Urls.host}users/` + id);
-  }
+    delete(id: number) {
+        return this.http.delete(`${Urls.host}users/` + id);
+    }
 }
