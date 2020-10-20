db5d5f8fdf3757848b070b023373f3090489b15f
= <=varuninorbit@yahoo.co.in>
2020-10-20 10:49:47 +0530

    section editable done

--- src/app/scheme/lavi/lavi.component.ts
+++ src/app/scheme/lavi/lavi.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-lavi',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lavi.component.css']
 })
 export class LaviComponent implements OnInit {
+  @Input() contenteditable;
+
   showPluti = {
     chapter: false,
     marks: false,
@@ -15,7 +17,24 @@ export class LaviComponent implements OnInit {
   laviState: namespace.LaviState = {
     sections: [
       {
-        name: 'Section A', showPluti: false,
+        name: 'Section A', showPluti: false, edit:false,
+        categories: [
+          {
+            name: 'MCQ', showPluti: false,
+            marks: [
+              {
+                value: '2', showPluti: false,
+                chapters: [
+                  { name: 'Mottion in 2D', no: '3', questionsNo: 4, showPluti: false },
+                  { name: 'Mottion in 3D', no: '4', questionsNo: 4, showPluti: false }
+                ]
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: 'Section A', showPluti: false, edit:false,
         categories: [
           {
             name: 'MCQ', showPluti: false,
@@ -34,15 +53,46 @@ export class LaviComponent implements OnInit {
     ]
   }
 
-  constructor() { }
+  constructor() { 
+    window['lavi']= this;
+  }
 
   ngOnInit(): void {
   }
 
-  click(...event) {
-    console.log(event);
+  click(obj:any, i:number, whichButton:string): void {
+    if(whichButton==='cross'){
+      this.delete(obj,i)
+    }
+
+    if(whichButton==='plus'){
+      this.add(obj,i)
+    }
   }
 
+  delete(obj:any,i:number){
+    obj.splice(i,1);
+  }
+
+  add(obj:any,i:number){
+    console.log('adding',i)
+  }
+
+  click2(obj:any, i:number, whichButton:string, sectionName): void {
+    if(whichButton==='plus'){
+     // this.add(obj,i)
+     console.log(sectionName);
+    }
+  }
+
+  edit(element,section){
+    section.edit=false;
+    section.name= element.innerText;
+  }
+  
+  toggleEdit(whatever){
+    whatever.edit=!whatever.edit
+  }   
 }
 
 declare module namespace {
@@ -70,9 +120,11 @@ declare module namespace {
     name: string;
     showPluti: boolean;
     categories: Category[];
+    edit: boolean;
   }
 
   export interface LaviState {
     sections: Section[];
   }
+  
 }
