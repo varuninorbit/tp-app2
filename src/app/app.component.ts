aed7ecd8ecb1ca7bf9a219d719fc592dbb3ef556
= <=varuninorbit@yahoo.co.in>
2020-11-04 13:10:33 +0530

    exam choice name issue resolved. It is now taking its value from state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,46 +8,48 @@ import { store } from 'src/app/_services/store.service.js';
 import { StateService } from './state.service';
 import { GlobalService } from './_services';
 import { RootStateService } from './root-state.service';
+import { ExamChoiceStateService } from './exam-choice/exam-choice-state.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, AfterViewInit  {
-  @ViewChild(MatSidenav,{static:true}) 
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild(MatSidenav, { static: true })
   menuRef;
 
-  examChoiceName:string;
-  constructor(private menu:ToggleMenuService, private zone: NgZone, 
-    private as:ActionService,
-    private user:UserService,
-    private state:StateService,
-    private gs:GlobalService,
-    private rootStateService: RootStateService
-    ){
-      window['app'] = this;
-      //let choiceName = this.gs.literal.resolve(store)('currentChoice.name'); //TODO fix code to update exam choice
-      let choiceName = this.state.state_.currentChoice.name;
-      if(!choiceName){
-        this.examChoiceName = 'Exam Choice';
-      }
+  examChoiceName: string;
+  constructor(private menu: ToggleMenuService, private zone: NgZone,
+    private as: ActionService,
+    private user: UserService,
+    private state: StateService,
+    private gs: GlobalService,
+    private rootStateService: RootStateService,//TODO: Optmise: analysis inection.
+    private examChoiceState: ExamChoiceStateService //TODO: Optmise: Shouln't be here technically
+  ) {
+    window['app'] = this;
+    //let choiceName = this.gs.literal.resolve(store)('currentChoice.name'); //TODO: Optmise fix code to update exam choice      
   }
 
-  ngOnInit(){
+  ngOnInit() {
     //this.menu.opened
-    window['appComp']=this;
-    this.state.state$.subscribe(state=>{
+    window['appComp'] = this;
+    this.state.state$.subscribe(state => {
       this.examChoiceName = state.currentChoice.name
     })
+
+    this.examChoiceState.state$.subscribe(state => {
+      this.examChoiceName = state.currentChoice.name;
+    });
   }
   mode = new FormControl('over');
 
-  menuToggle(sidenav){
+  menuToggle(sidenav) {
     sidenav.toggle();
   }
 
-  ngAfterViewInit(){
-    
+  ngAfterViewInit() {
+
   }
 }
