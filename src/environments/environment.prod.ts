67f96bb83cd063599cf25a080814c7ce7c0035be
varun sharma <varuninorbit@yahoo.co.in>
2021-01-11 11:19:16 +0530

    some tweaks

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,10 +5,6 @@ export const environment = {
 
 export const Urls = {
   host: "http://127.0.0.1/",
-  //questionsAPI:"http://test.test/questions/exp/questionv2?&std=8&sub=mat&p_no=10&q_nos=10&ch=2",
-  
-  //base: "http://192.168.0.103:3001/"
-  //for action
   base: "http://qpw_manage.test/api",
   relativeURL: "authi/"
 }
