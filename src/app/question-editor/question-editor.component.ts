971a35becb06db0950d007c4d251e9757c294b77
varun sharma <varuninorbit@yahoo.co.in>
2021-03-02 09:36:07 +0530

    some changes

--- src/app/question-editor/question-editor.component.ts
+++ src/app/question-editor/question-editor.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./question-editor.component.css']
 })
 export class QuestionEditorComponent implements OnInit {
-  editor='';
+  init = {
+    height: 500,
+    menubar: false,
+    plugins: [
+      'advlist autolink lists link image charmap print preview anchor',
+      'searchreplace visualblocks code fullscreen',
+      'insertdatetime media table paste code help wordcount'
+    ],
+    toolbar:
+      'undo redo | formatselect | bold italic backcolor | \
+      alignleft aligncenter alignright alignjustify | \
+      bullist numlist outdent indent | removeformat | help',
+    paste_data_images: true
+  }
+  editor = '';
   constructor() {
-    window['qedit']=this;
-   }
+    window['qedit'] = this;
+  }
 
   ngOnInit(): void {
   }
