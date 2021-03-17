3c88343c2e010efcba5f30654550a9d6524a4529
varun sharma <varuninorbit@yahoo.co.in>
2021-03-17 10:37:31 +0530

    chapter loading bug fixed

--- src/app/exam-choice/exam-choice-add/exam-choice-attributes.service.ts
+++ src/app/exam-choice/exam-choice-add/exam-choice-attributes.service.ts
@@ -1,13 +1,15 @@
 //TODO Obselete file
 import { Injectable } from "@angular/core";
 import { Observable, of } from "rxjs";
+import { RootStateService } from "src/app/root-state.service";
 import { Action2Service } from 'src/app/services/action2.service';
 
 @Injectable()
 export class ExamChoiceAttributesService {
-    observable: Observable<any>; //TODO change type
-  
-  constructor(private ac:Action2Service) {
-    this.observable  = ac.get({apiGroup:'8th_mat_cb_en',cache:'&cache',keyval:true})('AExamChoice.attributes')();   //TODO: change apiGroup
+  observable: Observable<any>; //TODO change type
+
+  constructor(private ac: Action2Service, private rs: RootStateService) {
+    let exam_choice = this.rs.getSateOf('examChoice').currentChoice.db_prefix
+    this.observable = ac.get({ apiGroup: exam_choice, cache: '&cache', keyval: true })('AExamChoice.attributes')();   //TODO: change apiGroup
   }
 }
